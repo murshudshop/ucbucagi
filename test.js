e27be8d3b6d37698b9e8ee7ad28e8fc64f239911
murshud <murshud@gmail.com>
2023-07-08 11:28:45 +0400

    salam

---
+++ test.js
@@ -0,0 +1,8 @@
+
+
+let a = +prompt('ucbucaglinin a terfeni daxil edin')
+let b = +prompt('ucbucaglinin b terefini daxil edin')
+let c = +prompt('ucbucaglinin c terefini daxil edin')
+
+let pr = a + b + c ;
+document.getElementById('zeki').innerHTML = ' A terefi ' + a + ' sm olan ucbucagin, B terefi ' + b + ' sm , c terefi ise ' + c + ' sm olarsa , bu ucbucagin perimetri ' + pr + ' sm olar. '
